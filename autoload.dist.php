55f629ac64e8426701cfe1e161b5cf8d072f120f
Rasso Hilber <mail@rassohilber.com>
2025-02-06 15:35:05 +0100

    Optimize code comments

--- autoload.dist.php
+++ autoload.dist.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Hirasso\FocalPointPicker;
 
 /**
- * A custom Autoloader to replace composer's during distribution.
+ * A custom Autoloader to replace the composer autoloader in production.
  * Otherwise, we would need to commit the vendor folder to git to
  * be able to install the plugin without composer.
  */
@@ -25,7 +25,7 @@ spl_autoload_register(function (string $class): void {
 
     /**
      * Replace namespace separators with directory separators and append '.php'
-     * NOT using DIRECTORY_SEPARATOR as PHP normalized the path internally (for e.g. Windows)
+     * NOT using DIRECTORY_SEPARATOR as PHP normalizes the path internally (for e.g. Windows)
      * DIRECTORY_SEPARATOR is meant for interacting with other tools outside of PHP.
      */
     $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
